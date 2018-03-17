77e93eb697f8771a94f2329adaa2d83f7a300a83
Seymour-Lee <894951782@qq.com>
2018-03-17 18:50:36 +0800

    add rebuild.sh script to rebuild and reinstall tensorflow

---
+++ rebuild.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+bazel build --config=opt --incompatible_load_argument_is_label=false //tensorflow/tools/pip_package:build_pip_package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+sudo pip uninstall tensorflow
+sudo pip install /tmp/tensorflow_pkg/tensorflow-1.4.1-cp27-cp27m-macosx_10_13_intel.whl
