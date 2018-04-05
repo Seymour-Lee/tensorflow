72279de8d64400d2a80a875fb6f7caa01ce7c8ce
Seymour-Lee <894951782@qq.com>
2018-04-05 09:18:24 +0800

    Fix rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -5,5 +5,5 @@ set -o pipefail
 
 bazel build --config=opt --incompatible_load_argument_is_label=false //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-sudo pip uninstall tensorflow
+sudo pip uninstall tensorflow -y
 sudo pip install /tmp/tensorflow_pkg/tensorflow-1.4.1-cp27-cp27m-macosx_10_13_intel.whl
